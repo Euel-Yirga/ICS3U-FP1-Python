bc211cc2cde1d7c6d531d7287db98295f1c4e6aa
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2019-10-09 16:45:21 +0000

    whats up

---
+++ pybadge_hello, world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 3
+
+# Created by: Euel Yirga
+# Created on: Sept 2019
+# this is the "Hello, World!" program on the pybadge
+
+
+def main():
+
+    # this function prints to the screen and console
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    while True:
+        print("What's up")
+        # repeat forever or you turn it off
+        pass  # just a place holder
+
+
+if __name__ == "__main__":
+    main()
